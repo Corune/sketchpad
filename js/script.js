0074e56c2bbcc1f1e7f04e809f0392d49178f5c5
Corune <stovall.mail@gmail.com>
2015-05-19 17:28:35 -0400

    added clear and reset buttons as well as table generation

--- js/script.js
+++ js/script.js
@@ -1,21 +1,54 @@
+// A function to handle the generation of the table
+var genTable = function(table, dimensions) {
+    var tableScheme = "";
+    for (var i = 0; i < dimensions; i++) {
+	tableScheme += "<tr>";
+	for (var j = 0; j < dimensions; j++)
+	    tableScheme += "<td></td>";
+	tableScheme += "</tr>";
+    }
+    table.append(tableScheme);
+};
+
+var resetTable = function(table, dimensions) {
+    table.empty();
+    genTable(table, dimensions);
+};
+
+// A function to handle clearing the table
+var clearTable = function() {
+    $("td").removeClass("clicked");
+};
+
 $(document).ready(function(){
     var isDown = false;
-    var table = $(document);
+    var doc = $(document);
+    var table = $("table");
 
-    table.mousedown(function(){
+    // Generating the table
+    genTable(table, 50);
+
+    // Reseting the sketchpad when #reset is clicked
+    $("#reset").mouseup(function(){
+	var resolution = +prompt("What resolution would you like the square?");
+	resetTable(table, resolution);
+    });
+
+    // Clear table when #clear is clicked
+    $("#clear").mouseup(clearTable);
+
+    // Checking to see if mouse is clicked down
+    doc.mousedown(function(){
 	isDown = true;
     }).mouseup(function(){
 	isDown = false;
     });
 
-    $("td").mouseover(function(event){
+    // If mouse is down, start drawing, if not, don't
+    table.on("mouseover", "td", function(event){
 	event.preventDefault();
 	thisOne = $(this);
-	if (isDown) {
-	    if (thisOne.hasClass("clicked"))
-		thisOne.removeClass("clicked");
-	    else
-		thisOne.addClass("clicked");
-	}
+	if (isDown)
+	    thisOne.addClass("clicked");
     });
 });
